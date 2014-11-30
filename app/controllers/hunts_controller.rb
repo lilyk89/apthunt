f39cf62034e416683b1864b8da44122add031fdd
lilyk89 <lilyk89@gmail.com>
2014-11-30 17:56:27 -0500

    After office hours, remove and add users now work, kind of

--- app/controllers/hunts_controller.rb
+++ app/controllers/hunts_controller.rb
@@ -1,6 +1,6 @@
 class HuntsController < ApplicationController
   before_action :authenticate_user!, only: [:index, :show, :edit, :create, :update, :destroy]
-  before_action :set_hunt, only: [:show, :edit, :update, :destroy, :add_user]
+  before_action :set_hunt, only: [:show, :edit, :update, :destroy, :add_user, :remove_user]
 
   # GET /hunts
   # GET /hunts.json
@@ -22,8 +22,41 @@ class HuntsController < ApplicationController
   def edit
   end
 
-# Doesnt actually do anything right now... why?
+# PATCH/PUT /hunts/1/add_user
   def add_user
+    new_user = User.where email: params[:q]
+    if @hunt.users.include? new_user
+      @hunt.users.push new_user
+      respond_to do |format|
+        if @hunt.save
+          format.html { redirect_to @hunt, notice: 'Hunt was successfully updated.' }
+          format.json { render :show, status: :ok, location: @hunt }
+        else
+          format.html { render :edit }
+          format.json { render json: @hunt.errors, status: :unprocessable_entity }
+        end
+      end
+    else
+      respond_to do |format|
+        format.html { render :edit }
+        format.json { render json: @hunt.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  #
+  def remove_user
+    user_to_remove = User.where email: params[:r]
+    @hunt.users.delete(user_to_remove)
+    respond_to do |format|
+      if @hunt.save
+        format.html { redirect_to @hunt, notice: 'Hunt was successfully updated.' }
+        format.json { render :show, status: :ok, location: @hunt }
+      else
+        format.html { render :edit }
+        format.json { render json: @hunt.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   # POST /hunts
@@ -32,8 +65,9 @@ class HuntsController < ApplicationController
     if user_signed_in?
     @hunt = Hunt.new(hunt_params)
     @hunt.users << @current_user
-  else puts "Log in first"
-  end
+    else
+    puts "Log in first"
+      end
 
     respond_to do |format|
       if @hunt.save
@@ -75,13 +109,12 @@ private
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def hunt_params
-      params.require(:hunt).permit(:name, :admin, :description, :users,
+      params.require(:hunt).permit(:name, :admin, :description, :users, :q,
         apt_attributes: [:id, :_destroy],
         user_attributes:[:id]) 
-      if params[:hunt]
     end
 
     def set_hunt
       @hunt = Hunt.find(params[:id])
     end
-  end
+end
